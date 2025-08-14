76191b393defdd74157db80624969f8383d7b784
Gabriel Patano Kiyot <gabrielkiyoto@unifev.corp.br>
2025-08-14 12:34:16 -0300

    configurando o phpMailer

--- processa_envio.php
+++ processa_envio.php
@@ -46,24 +46,23 @@
 			//Server settings
 			$mail->SMTPDebug = 2;                      //Enable verbose debug output
 			$mail->isSMTP();                                            //Send using SMTP
-			$mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+			$mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
 			$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-			$mail->Username   = 'user@example.com';                     //SMTP username
-			$mail->Password   = 'secret';                               //SMTP password
-			$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+			$mail->Username   = 'webcompleto2@gmail.com';                     //SMTP username
+			$mail->Password   = '!@#$4321';                               //SMTP password
+			$mail->SMTPSecure = 'tls';         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 			$mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
 			//Recipients
-			$mail->setFrom('from@example.com', 'Mailer');
-			$mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
-			$mail->addAddress('ellen@example.com');               //Name is optional
-			$mail->addReplyTo('info@example.com', 'Information');
-			$mail->addCC('cc@example.com');
-			$mail->addBCC('bcc@example.com');
+			$mail->setFrom('webcompleto2@gmail.com', 'Web Completo Remetente');
+			$mail->addAddress('webcompleto2@gmail.com', 'web Completo Destinatário');     //Add a recipient
+			//$mail->addReplyTo('info@example.com', 'Information');
+			//$mail->addCC('cc@example.com');
+			//$mail->addBCC('bcc@example.com');
 
 			//Attachments
-			$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-			$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+			//$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+			//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
 
 			//Content
 			$mail->isHTML(true);                                  //Set email format to HTML
