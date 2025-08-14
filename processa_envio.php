feb26c3a0c2de70bdeec38ec0f2442fbf0eb0637
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-08-14 10:31:50 -0300

    php mail correção

--- processa_envio.php
+++ processa_envio.php
@@ -1,4 +1,10 @@
 <?php
+    require "./bibliotecas/PHPMailer/Exception.php";
+    require "./bibliotecas/PHPMailer/OAuth.php";
+    require "./bibliotecas/PHPMailer/PHPMailer.php";
+    require "./bibliotecas/PHPMailer/POP3.php";
+    require "./bibliotecas/PHPMailer/SMTP.php";
+
     class Mensagem {
         private $para = null;
         private $assunto = null;
